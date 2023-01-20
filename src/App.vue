19979e816befa1a5e57e8ce86f6b8f8f3ad28015
shogo4131 <shougo08697118@gmail.com>
2023-01-20 15:57:49 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script lang="tsx">
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'App',
+  setup() {
+    return () => (
+      <>
+        <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+      </>
+    );
+  },
+});
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: { open: true },
+  plugins: [vue(), vueJsx()],
+});
