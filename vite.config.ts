bf683a71d8ce22d43d4e5b6d42fc7dcf081af591
shogo4131 <shougo08697118@gmail.com>
2023-01-25 14:09:55 +0900

    feat: add elias import setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
+import { resolve } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { open: true },
   plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': resolve('./src'),
+    },
+  },
 });
